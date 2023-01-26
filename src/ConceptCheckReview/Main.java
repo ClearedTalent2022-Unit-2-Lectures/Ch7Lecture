8bb67ad832f1919e8caf11488c3bc37bbb22a613
pearce-odegard <pearceodegard@gmail.com>
2023-01-26 14:08:13 -0600

    Added Concept Check review package

---
+++ src/ConceptCheckReview/Main.java
@@ -0,0 +1,9 @@
+package ConceptCheckReview;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Greeting hello = new Greeting("English", "Hello Coder!");
+        hello.wave();
+    }
+}
